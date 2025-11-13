b4865c692f9229e834a6c0f2705134b2b99c166c
root <root@vmi2899923.contaboserver.net>
2025-11-13 18:09:58 +0100

    issue server.js backoffice

--- apps/backoffice/server.js
+++ apps/backoffice/server.js
@@ -1,5 +1,10 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT || 3006;
 
@@ -7,7 +12,7 @@ const PORT = process.env.PORT || 3006;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // For all other routes, serve the index.html file
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
